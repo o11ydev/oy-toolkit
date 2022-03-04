2adc0a8445fe663c2ae9fb35c23e8ccdcc3fec93
Julien Pivotto <roidelapluie@inuits.eu>
2022-03-04 16:30:15 +0100

    Remove sha
    Signed-off-by: Julien Pivotto <roidelapluie@inuits.eu>

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
 
       src = ./.;
 
-      vendorSha256 = "sha256-pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
+      vendorSha256 = null;
     };
     devShell = pkgs.mkShell {
       buildInputs = [

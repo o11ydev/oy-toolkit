c8a73dc022ee634055322b209516a7aa43106ad5
Julien Pivotto <roidelapluie@inuits.eu>
2022-03-14 21:30:50 +0100

    Format packages.nix
    Signed-off-by: Julien Pivotto <roidelapluie@inuits.eu>

--- packages.nix
+++ packages.nix
@@ -1,34 +1,34 @@
 { pkgs, ... }: with pkgs;
 let
   basepkg = name: buildGo117Module.override
-      {
-        go = go.overrideAttrs (oldAttrs: rec {
-          version = "1.17.8";
-          src = fetchurl {
-            url = "https://dl.google.com/go/go${version}.src.tar.gz";
-            sha256 = "sha256-Lv/NiYFA2nmgYfN4TKT42LE9gR+yq+na0kBEQtq733o=";
-          };
-        });
-      }
-      {
-        name = name;
-        src = ./.;
-        vendorSha256 = "sha256-0jqcIE+BZtj7Z7/C0sEPx0acSm0Dv6YxqVGklqmLweA=";
-        subPackages = if name == "oy-toolkit" then [] else [ "./cmd/${name}" ];
-        nativeBuildInputs = [ (import ./go.nix { inherit pkgs; }) ];
+    {
+      go = go.overrideAttrs (oldAttrs: rec {
+        version = "1.17.8";
+        src = fetchurl {
+          url = "https://dl.google.com/go/go${version}.src.tar.gz";
+          sha256 = "sha256-Lv/NiYFA2nmgYfN4TKT42LE9gR+yq+na0kBEQtq733o=";
+        };
+      });
+    }
+    {
+      name = name;
+      src = ./.;
+      vendorSha256 = "sha256-0jqcIE+BZtj7Z7/C0sEPx0acSm0Dv6YxqVGklqmLweA=";
+      subPackages = if name == "oy-toolkit" then [ ] else [ "./cmd/${name}" ];
+      nativeBuildInputs = [ (import ./go.nix { inherit pkgs; }) ];
 
-        ldflags = [
-          "-X github.com/prometheus/common/version.Version=${builtins.readFile ./VERSION}"
-          "-X github.com/prometheus/common/version.Branch=n/a"
-          "-X github.com/prometheus/common/version.Revision=n/a"
-          "-X github.com/prometheus/common/version.BuildUser=${builtins.readFile (pkgs.runCommand "whoami" { } ("whoami > $out"))}"
-          "-X github.com/prometheus/common/version.BuildDate=${builtins.readFile (pkgs.runCommand "date" { } ("date -u '+%Y%m%d-%H:%M:%S%p' > $out"))}"
-        ];
-      };
+      ldflags = [
+        "-X github.com/prometheus/common/version.Version=${builtins.readFile ./VERSION}"
+        "-X github.com/prometheus/common/version.Branch=n/a"
+        "-X github.com/prometheus/common/version.Revision=n/a"
+        "-X github.com/prometheus/common/version.BuildUser=${builtins.readFile (pkgs.runCommand "whoami" { } ("whoami > $out"))}"
+        "-X github.com/prometheus/common/version.BuildDate=${builtins.readFile (pkgs.runCommand "date" { } ("date -u '+%Y%m%d-%H:%M:%S%p' > $out"))}"
+      ];
+    };
 in
-  lib.recursiveUpdate { oy-toolkit = (basepkg "oy-toolkit"); }
-(builtins.mapAttrs
-  (name: value:
-  basepkg name
-      )
-      (builtins.readDir ./cmd))
+lib.recursiveUpdate { oy-toolkit = (basepkg "oy-toolkit"); }
+  (builtins.mapAttrs
+    (name: value:
+      basepkg name
+    )
+    (builtins.readDir ./cmd))

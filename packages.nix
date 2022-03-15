2d052572d0403c7c0f22a03a2880914ec66d86cd
Julien Pivotto <roidelapluie@inuits.eu>
2022-03-15 17:51:34 +0100

    Avoid rebuilding go for the moment
    Signed-off-by: Julien Pivotto <roidelapluie@inuits.eu>

--- packages.nix
+++ packages.nix
@@ -2,13 +2,13 @@
 let
   basepkg = name: buildGo117Module.override
     {
-      go = go.overrideAttrs (oldAttrs: rec {
-        version = "1.17.8";
-        src = fetchurl {
-          url = "https://dl.google.com/go/go${version}.src.tar.gz";
-          sha256 = "sha256-Lv/NiYFA2nmgYfN4TKT42LE9gR+yq+na0kBEQtq733o=";
-        };
-      });
+#      go = go.overrideAttrs (oldAttrs: rec {
+#        version = "1.17.8";
+#        src = fetchurl {
+#          url = "https://dl.google.com/go/go${version}.src.tar.gz";
+#          sha256 = "sha256-Lv/NiYFA2nmgYfN4TKT42LE9gR+yq+na0kBEQtq733o=";
+#        };
+#      });
     }
     {
       name = name;

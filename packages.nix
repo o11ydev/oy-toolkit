501aa59114d0c22f1f7fd0c65b51b7d6a85283cf
Julien Pivotto <roidelapluie@inuits.eu>
2022-03-15 11:51:04 +0100

    Remove some info
    Signed-off-by: Julien Pivotto <roidelapluie@inuits.eu>

--- packages.nix
+++ packages.nix
@@ -15,13 +15,14 @@ let
       src = ./.;
       vendorSha256 = "sha256-0jqcIE+BZtj7Z7/C0sEPx0acSm0Dv6YxqVGklqmLweA=";
       subPackages = if name == "oy-toolkit" then [ ] else [ "./cmd/${name}" ];
+      preBuild = "export buildDate=$(date -u '+%Y%m%d-%H:%M:%S%p'); echo OK > VERSION";
 
       ldflags = [
         "-X github.com/prometheus/common/version.Version=${builtins.readFile ./VERSION}"
         "-X github.com/prometheus/common/version.Branch=n/a"
         "-X github.com/prometheus/common/version.Revision=n/a"
-        "-X github.com/prometheus/common/version.BuildUser=${builtins.readFile (pkgs.runCommand "whoami" { } ("whoami > $out"))}"
-        "-X github.com/prometheus/common/version.BuildDate=${builtins.readFile (pkgs.runCommand "date" { } ("date -u '+%Y%m%d-%H:%M:%S%p' > $out"))}"
+        "-X github.com/prometheus/common/version.BuildUser=n/a}"
+        "-X github.com/prometheus/common/version.BuildDate=n/a}"
       ];
     };
   packageList = (builtins.mapAttrs

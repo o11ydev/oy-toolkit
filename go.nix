2db259cbcdde85c02a3eee75dfa4e2c79291430a
Julien Pivotto <roidelapluie@inuits.eu>
2022-03-16 23:54:27 +0100

    Complete go override
    Signed-off-by: Julien Pivotto <roidelapluie@inuits.eu>

--- go.nix
+++ go.nix
@@ -1,11 +1,14 @@
-{
+{pkgs, ...}: let
+  go = pkgs.go_1_18.overrideAttrs (oldAttrs: rec {
+    version = "1.18";
+    src = pkgs.fetchurl {
+      url = "https://dl.google.com/go/go${version}.src.tar.gz";
+      sha256 = "sha256-OPQj20zINIg/K1I0QoL6ejn7uTZQ3GKhH98L5kCb2tY=";
+    };
+  });
+in {
   packageOverrides = pkgs: {
-    go = pkgs.go_1_18.overrideAttrs (oldAttrs: rec {
-      version = "1.18";
-      src = pkgs.fetchurl {
-        url = "https://dl.google.com/go/go${version}.src.tar.gz";
-        sha256 = "sha256-OPQj20zINIg/K1I0QoL6ejn7uTZQ3GKhH98L5kCb2tY=";
-      };
-    });
+    buildGoModule = pkgs.buildGoModule.override {go = go;};
+    go = go;
   };
 }

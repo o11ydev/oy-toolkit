d266826bcd788c7701734f72aa59665876c4c95d
Julien Pivotto <roidelapluie@o11y.eu>
2022-04-27 00:40:32 +0200

    Use go 1.18 from nix (#17)
    Signed-off-by: Julien Pivotto <roidelapluie@o11y.eu>

--- go.nix
+++ go.nix
@@ -1,11 +1,5 @@
 {pkgs, ...}: let
-  go = pkgs.go_1_18.overrideAttrs (oldAttrs: rec {
-    version = "1.18";
-    src = pkgs.fetchurl {
-      url = "https://dl.google.com/go/go${version}.src.tar.gz";
-      sha256 = "sha256-OPQj20zINIg/K1I0QoL6ejn7uTZQ3GKhH98L5kCb2tY=";
-    };
-  });
+  go = pkgs.go_1_18;
 in {
   packageOverrides = pkgs: {
     buildGoModule = pkgs.buildGoModule.override {go = go;};
